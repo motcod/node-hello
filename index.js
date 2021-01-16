b8a5663778534d9a3884ecd3e08597592fa83003
motcod <77477056+motcod@users.noreply.github.com>
2021-01-17 00:00:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  const msg = 'Hello Yoni. I am looking for your Playstation!\n'
   res.end(msg);
 });
 
